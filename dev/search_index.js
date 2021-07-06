4a65c910beae304c2d35071224b8cf17f05908c2
Documenter.jl <documenter@juliadocs.github.io>
2021-07-06 06:55:11 +0000

    build based on d64b51f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FairnessInprocessing","category":"page"},{"location":"#FairnessInprocessing","page":"Home","title":"FairnessInprocessing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FairnessInprocessing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FairnessInprocessing]","category":"page"}]
+}
